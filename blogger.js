fbdb403d886888ae92179c7e30dbcdf152a5150c
mockupz <131960727+mockupz@users.noreply.github.com>
2023-05-06 08:15:50 +0700

    Update blogger.js

--- blogger.js
+++ blogger.js
@@ -1,5 +1,5 @@
 var ars = rChoice([
-  'https://camerawebpage.com','https://autocareview.com','https://Recipewiki.eu.org','https://camerawebpage.com',
+  'https://camerawebpage.com','https://autocareview.com','https://Recipewiki.eu.org',
   ]);
 
 function rChoice(arr) {
